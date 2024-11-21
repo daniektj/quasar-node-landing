323360dbda319750ecce5cefd87714cd55011d42
DaniekTj <65110807+daniektj@users.noreply.github.com>
2024-11-20 23:33:15 -0800

    ✏️ [TYPO] se cambio el texto de contacto

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,7 +3,7 @@ export const Header = () => {
   return (
     <div className='w-full flex justify-between items-center'>
         <a href="#"><img src="/images/logo-quasar.svg" alt="logo quasar node" /></a>
-        <a href="#" className="hover:text-[#0D97DF]">Contactanos</a>
+        <a href="#" className="hover:text-[#0D97DF]">Contact Us</a>
     </div>
   )
 }
