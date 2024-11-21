3ac45e674b0ebdae81bfd9e722dc815335eb6801
DaniekTj <65110807+daniektj@users.noreply.github.com>
2024-11-21 05:01:47 -0800

    🎨[DESIGN] se elimino el contacto en header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
     <div className='w-full flex justify-between items-center'>
         <a href="#"><img src="/images/logo-quasar.svg" alt="logo quasar node" /></a>
         <div className='flex items-center gap-8'>
-          <a href="#" className="hover:text-[#0D97DF]">Contact Us</a>
+          {/* <a href="#" className="hover:text-[#0D97DF]">Contact Us</a> */}
           <a href="https://discord.gg/8aUd3ykG" className="bg-white text-black px-4 py-1 rounded-md hover:text-[#0D97DF] font-bold">Discord</a>
         </div>
     </div>
