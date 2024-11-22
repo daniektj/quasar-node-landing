7fbaa96b0d5cc470ac3271083eb3b7e6a3e7420f
DaniekTj <65110807+daniektj@users.noreply.github.com>
2024-11-22 03:08:56 -0800

    🎨[DESIGN] se cambio el diseño en footer v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -107,13 +107,6 @@ function HomePage() {
         </div>
       </section>
 
-      {/* <Footer /> */}
-
-{/*       <section id="botones">
-        <button className=" rounded-lg relative h-12 w-40 overflow-hidden border border-white text-white shadow-2xl transition-all duration-200 hover:border-[#0D97DF] before:absolute before:bottom-0 before:left-0 before:right-0 before:top-0 before:m-auto before:h-0 before:w-0 before:rounded-sm before:bg-[#0D97DF] before:duration-300 before:ease-out hover:text-white hover:shadow-[#0D97DF] hover:before:h-40 hover:before:w-40 hover:before:opacity-80">
-          <span className="relative z-10 text-xl">Buy Now</span>
-        </button>
-      </section> */}
 
     </div>
   )
--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,8 +1,5 @@
 export const Footer = () => {
     return (
-        /*     <div className="w-full h-14 flex items-center justify-center mt-20">
-                <img src="/images/logo-quasar.svg" alt="logo quasar node" className='h-8' />
-            </div> */
         <>
             <img src="/images/bg-footer-top.png" className='mt-[-64px]' />
             <div className="w-full flex items-center justify-center bg-gradient-to-b from-[#181A2B] to-[#0E1013] h-[104px] border-t-2 border-[#282C4D]">
@@ -12,11 +9,3 @@ export const Footer = () => {
     )
 }
 
-
-
-
-
-
-
-
-/* border-t-2 border-[#282C4D] */
